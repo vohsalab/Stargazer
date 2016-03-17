de08c29236710882413522c7d7ea997e4be4059f
Ivan Balashov <sputnic1985@gmail.com>
2016-03-16 23:02:36 -0400

    added fragments and model for apod api call

---
+++ app/src/main/java/com/firstsputnic/stargazer/Model/Apod.java
@@ -0,0 +1,182 @@
+package com.firstsputnic.stargazer.Model;
+
+/**
+ * Created by ibalashov on 3/16/2016.
+ */
+
+    import com.google.gson.annotations.Expose;
+    import com.google.gson.annotations.SerializedName;
+
+    public class Apod {
+
+        @SerializedName("copyright")
+        @Expose
+        private String copyright;
+        @SerializedName("date")
+        @Expose
+        private String date;
+        @SerializedName("explanation")
+        @Expose
+        private String explanation;
+        @SerializedName("hdurl")
+        @Expose
+        private String hdurl;
+        @SerializedName("media_type")
+        @Expose
+        private String mediaType;
+        @SerializedName("service_version")
+        @Expose
+        private String serviceVersion;
+        @SerializedName("title")
+        @Expose
+        private String title;
+        @SerializedName("url")
+        @Expose
+        private String url;
+
+        /**
+         *
+         * @return
+         * The copyright
+         */
+        public String getCopyright() {
+            return copyright;
+        }
+
+        /**
+         *
+         * @param copyright
+         * The copyright
+         */
+        public void setCopyright(String copyright) {
+            this.copyright = copyright;
+        }
+
+        /**
+         *
+         * @return
+         * The date
+         */
+        public String getDate() {
+            return date;
+        }
+
+        /**
+         *
+         * @param date
+         * The date
+         */
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        /**
+         *
+         * @return
+         * The explanation
+         */
+        public String getExplanation() {
+            return explanation;
+        }
+
+        /**
+         *
+         * @param explanation
+         * The explanation
+         */
+        public void setExplanation(String explanation) {
+            this.explanation = explanation;
+        }
+
+        /**
+         *
+         * @return
+         * The hdurl
+         */
+        public String getHdurl() {
+            return hdurl;
+        }
+
+        /**
+         *
+         * @param hdurl
+         * The hdurl
+         */
+        public void setHdurl(String hdurl) {
+            this.hdurl = hdurl;
+        }
+
+        /**
+         *
+         * @return
+         * The mediaType
+         */
+        public String getMediaType() {
+            return mediaType;
+        }
+
+        /**
+         *
+         * @param mediaType
+         * The media_type
+         */
+        public void setMediaType(String mediaType) {
+            this.mediaType = mediaType;
+        }
+
+        /**
+         *
+         * @return
+         * The serviceVersion
+         */
+        public String getServiceVersion() {
+            return serviceVersion;
+        }
+
+        /**
+         *
+         * @param serviceVersion
+         * The service_version
+         */
+        public void setServiceVersion(String serviceVersion) {
+            this.serviceVersion = serviceVersion;
+        }
+
+        /**
+         *
+         * @return
+         * The title
+         */
+        public String getTitle() {
+            return title;
+        }
+
+        /**
+         *
+         * @param title
+         * The title
+         */
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        /**
+         *
+         * @return
+         * The url
+         */
+        public String getUrl() {
+            return url;
+        }
+
+        /**
+         *
+         * @param url
+         * The url
+         */
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+    }
+
--- app/src/main/java/com/firstsputnic/stargazer/View/MainActivity.java
+++ app/src/main/java/com/firstsputnic/stargazer/View/MainActivity.java
@@ -1,17 +1,20 @@
-package com.firstsputnic.stargazer;
+package com.firstsputnic.stargazer.View;
 
+import android.support.v4.app.FragmentManager;
 import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.firstsputnic.stargazer.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -31,31 +34,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meet_iss);
+        setContentView(R.layout.activity_main);
+        Fragment fragment = new MeetISSFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
         ButterKnife.bind(this);
         mTitle = mDrawerTitle = getTitle();
         mTabNames = getResources().getStringArray(R.array.tabs);
         mDrawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, mTabNames));
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-        });
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
                 new Toolbar(this), R.string.drawer_open, R.string.drawer_close) {
 
             /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
-                getSupportActionBar().setTitle(mTitle);
+                //getSupportActionBar().setTitle(mTitle);
 
             }
 
             /** Called when a drawer has settled in a completely open state. */
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle(mDrawerTitle);
+                //getSupportActionBar().setTitle(mDrawerTitle);
 
             }
         };
@@ -88,6 +91,40 @@ public class MainActivity extends AppCompatActivity {
         // Handle your other action bar items...
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+    private void selectItem(int position) {
+        //Toast.makeText(this, "Position:" + position, Toast.LENGTH_SHORT).show();
+        Fragment fragment;
+        switch (position) {
+            case 0:
+                fragment = new MeetISSFragment();
+                break;
+            case 1:
+                fragment = new ISSNowFragment();
+                break;
+            case 2:
+                fragment = new APODFragment();
+                break;
+            default:
+                fragment = new MeetISSFragment();
+                break;
+        }
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        getSupportActionBar().setTitle(mTabNames[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
 
 }
